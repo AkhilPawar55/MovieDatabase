8a880675aa33daa45698590f08a35c476fbc1fc9
Akhil Pawar <akhilpawar55@gmail.com>
2021-02-10 17:29:48 -0500

    WPF application now connected to local SQL Server Database

--- source/MovieDatabaseApp/MovieDatabaseApp/MovieDatabaseHome.xaml.cs
+++ source/MovieDatabaseApp/MovieDatabaseApp/MovieDatabaseHome.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,16 +19,44 @@ namespace MovieDatabaseApp
     /// <summary>
     /// Interaction logic for MovieDatabaseHome.xaml
     /// </summary>
-    public partial class MovieDatabaseHome : Page
+    public partial class MovieDatabaseHome : Window
     {
         public MovieDatabaseHome()
         {
             InitializeComponent();
         }
 
-        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
         {
+            SqlConnection sqlCon = new SqlConnection(@"Data Source=LAPTOP-AU4JA7HP; Initial Catalog=MovieDB; Integrated Security=True;");
+            try
+            {
+                if (sqlCon.State == System.Data.ConnectionState.Closed)
+                    sqlCon.Open();
+                String query = "SELECT COUNT(1) FROM tblMovie WHERE Name=@Name";
+                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+                sqlCmd.CommandType = System.Data.CommandType.Text;
+                sqlCmd.Parameters.AddWithValue("@Name", txtMovieName.Text);
+                int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+                if(count == 1){
 
+                    MainWindow movieDetails = new MainWindow();
+                    movieDetails.Show();
+                    this.Close();
+                }
+                else{
+
+                    MessageBox.Show("Movie you searched was not found in my database.");
+                }
+            }
+            catch (Exception ex){
+
+                MessageBox.Show(ex.Message);
+            }
+            finally{
+
+                sqlCon.Close();
+            }
         }
     }
 }
